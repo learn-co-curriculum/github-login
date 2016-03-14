90f242b8d07051f80f7ed4ee739e913dbd016855
JimCampagno <jimcampagno@gmail.com>
2016-03-14 13:20:03 -0400

    clean up name stuff

--- github-repo-starring/FISConstants.m
+++ github-repo-starring/FISConstants.m
@@ -9,6 +9,7 @@
 #import "FISConstants.h"
 
 @implementation FISConstants
+
 NSString *const GITHUB_CLIENT_SECRET=@"";
 NSString *const GITHUB_CLIENT_ID=@"";
 NSString *const GITHUB_ACCESS_TOKEN=@"";
--- github-repo-starring/FISGithubRepository.h
+++ github-repo-starring/FISGithubRepository.h
@@ -8,28 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-/**
- *  Class to represent a repository from Github.
- */
+
 @interface FISGithubRepository : NSObject
 
-///---------------------
-/// @name Properties
-///---------------------
 
-/**
-*  `NSString` full name of repository. Comes from the "full_name" key
-*/
 @property (strong, nonatomic) NSString *fullName;
-
-/**
- *  `NSURL` html URL of repository. Comes from the "html_url" key
- */
 @property (strong, nonatomic) NSURL *htmlURL;
-
-/**
- *  `NSString` repository ID. Comes from the "id" key
- */
 @property (strong, nonatomic) NSString *repositoryID;
 
 +(FISGithubRepository *)repoFromDictionary:(NSDictionary *)repoDictionary;
