60af38bf3223389671e958a3bee4acae426df482
JimCampagno <jimcampagno@gmail.com>
2016-03-14 13:22:45 -0400

    clean up methods

--- github-repo-starring/FISGithubAPIClient.h
+++ github-repo-starring/FISGithubAPIClient.h
@@ -8,20 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-NSString *const GITHUB_API_URL;
 @interface FISGithubAPIClient : NSObject
 
-///---------------------
-/// @name Class Methods
-///---------------------
-
-/**
- *  Does a `GET` request to the [Github repository API](https://developer.github.com/v3/repos/#list-all-public-repositories) with the `client_id` and `client_secret` parameters as well.
- *  @param completionBlock Block to be called when finished retreiving from the API. Passes an `NSArray` of `NSDictionary` objects from the API.
- */
-
 +(void)getRepositoriesWithCompletion:(void (^)(NSArray *repoDictionaries))completionBlock;
-+(void)checkIfRepoIsStarredWithFullName:(NSString *)fullName CompletionBlock:(void (^)(BOOL starred))completionBlock;
-+(void)starRepoWithFullName:(NSString *)fullName CompletionBlock:(void (^)(void))completionBlock;
-+(void)unstarRepoWithFullName:(NSString *)fullName CompletionBlock:(void (^)(void))completionBlock;
+
++(void)checkIfRepoIsStarredWithFullName:(NSString *)fullName
+                        completionBlock:(void (^)(BOOL starred))completionBlock;
+
++(void)starRepoWithFullName:(NSString *)fullName
+            completionBlock:(void (^)(void))completionBlock;
+
++(void)unstarRepoWithFullName:(NSString *)fullName
+              completionBlock:(void (^)(void))completionBlock;
+
 @end
